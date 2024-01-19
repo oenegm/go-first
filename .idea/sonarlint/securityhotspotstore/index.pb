959be8e5fe9838a92747468c1926fd8a2d86911d
oenegm <oenegm@gmail.com>
2024-01-19 13:57:15 +0200

    structs

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,24 +1,18 @@
 
 6
 go.mod,c\4\c47645c391ad0571c40779079363c9d48412e18b
-H
-helloworld/hello_test.go,3\d\3d88fce8327453c35d10d2823082bcef15bf1808
 F
 integers/adder_test.go,3\1\3127f27833572538a21f3b4aa63478859499def3
-C
-helloworld/hello.go,5\1\5123d50027601faf57d1d297ba76c67e8542c6ae
-?
-integers/add.go,7\9\795fd61a15cf8425ee05147f911e8e92a0f4f04b
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 A
 integers/adder.go,e\f\ef76cb63b2344f900dabae8fbc5c8928d87d3e5c
-H
-iteration/repeat_test.go,c\8\c896670e1e75d63d369744ace84c8d0ed090657b
-H
-iteration/repeatation.go,c\1\c1b92e937444ae50b481d8fd093fb7d44f2c2d96
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
 S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+A
+structs/shapes.go,0\1\015432afb146dfa4eb15f5d4dc0df1afe52d0682
+F
+structs/shapes_test.go,0\4\04e8df9868f2cc428553bc57bff69e0b1c85ebbe
---
+++ structs/shapes_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkPerimeter(t, rectangle, 40.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkPerimeter(t, circle, 62.83185307179586476925286766559)
+	})
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
